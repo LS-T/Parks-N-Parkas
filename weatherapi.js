8442ddee1f1f103e3f060031e77cfa205fe79c86
LS-T <leo.s.teruel@gmail.com>
2021-03-31 22:32:05 -0400

    testing icon

--- weatherapi.js
+++ weatherapi.js
@@ -1,10 +1,17 @@
 console.log("Hello world!");
 apiKey = "1ebd3e88b4147deeadc030e6248c294d";
-var requestURL = "https://pro.openweathermap.org/data/2.5/forecast/climate?q=Keystone&appid=" + apiKey;
-
+var city = "Atlanta";
+var lat = 33.7;
+var lon = -84.3;
+var temp = $("#temp");
+var humid = $("#humid");
+var numba = 10;
+var numbaString = numba.valueOf
+console.log(numbaString);
 
 function getWeatherApi () {
-    var requestURL = "https://pro.openweathermap.org/data/2.5/forecast/climate?q=Keystone&appid=" + apiKey;
+    var days = [0,1,2,3,4,5,6,7];
+    var requestURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon="+ lon + "&exclude=minutely,hourly&units=imperial&appid=" + apiKey;
 
     fetch(requestURL)
         .then(function(response){
@@ -12,6 +19,16 @@ function getWeatherApi () {
         })
         .then(function(data){
             console.log(data);
+            temp.text(data.current.temp);
+            humid.text(data.current.humidity);
+            $("#dayTemp").text(data.daily[0].temp.day);
+            $("#nightTemp").text(data.daily[0].temp.night);
+            var iconCode = data.daily[0].weather[0].icon;
+            console.log(iconCode);
+            var iconUrl = "https://openweathermap.org/img/wn/"+ iconCode + "@2x.png"
+            $('#wicon').attr('src',iconUrl);
+         
+
         })
 }
 getWeatherApi();
