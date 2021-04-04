480f1000330c5a5fa6a160f3dffe43d56c3b074a
Emily Wilbur <79462454+emilyjwilbur@users.noreply.github.com>
2021-04-04 17:18:53 -0400

    results are clickable, made dropdown of states in html (#37)
    * results are clickable, made dropdown of states in html
    
    * quick fix, no major changes

--- script.js
+++ script.js
@@ -1,61 +1,4 @@
 
-// // get form, searched park and list
-// var addToList = document.querySelector('#add-to-list');
-// var listItem = document.querySelector('#list-item');
-// var parkList = document.querySelector('#savedPark');
-
-// let savedParks = [];
-
-// addToList.addEventListener('submit', function (event) {
-
-//     // don't submit form
-//     event.preventDefault();
-
-//     // ignore if list item is empty
-//     if (listItem.value.length < 1)
-//     return;
-
-//     savedParks.push(listItem.value); // add park to list of saved parks
-
-//     buildParkList(); 
-
-//     // clear input
-//     listItem.value = '';
-
-//     // save list to local storage
-//     localStorage.setItem('savedParks', savedParks); // save the parks array to local storage
-
-// }, false);
-
-// // check for saved list items
-
-// function getParksFromStorage(){
-//     var searched = localStorage.getItem('savedParks');
-
-//     if (searched) {
-//         buildParksList();
-//     }
-// }
-
-// function buildParkList(){
-
-//     parkList.innerHTML = ''; // clear list of parks on the page
-
-//     if(savedParks != undefined) // check if there are parks saved in the array
-//     {
-//         let parks = savedParks;
-//         for(let x = 0; x < parks.length; x++){ // for each park in array create a list item and add to list on page
-//             let item = document.createElement('ul');
-//             item.innerHTML = parks[x];
-//             parkList.appendChild(item);
-//         }
-//     }   
-// }
-
-
-
-
-
 var searchFormEl = document.querySelector('#search-form');
 function handleSearchFormSubmit(event) {
   event.preventDefault();
@@ -82,14 +25,21 @@ function getFullName(latLong) {
       console.log(results);
       $("#search-results").empty();
       var dataArray = results.data;
+      let liEl = $("<ul>");
+      liEl.css("list-style", "none");
       for (var i = 0; i < dataArray.length; i++) {
-        var liEl = $("<ul>");
-        liEl.text(dataArray[i].name);
-        $("#search-results").append(liEl);
-   
+        let liButton = '<li><a href="#">' + dataArray[i].name +'</a></li>';
+        liEl.append(liButton);
       }
+
+        // var liEl = $("<li>");
+        // liEl.text(dataArray[i].name);
+        $("#search-results").append(liEl);
+      
     });
 }
+
+
 
 $("#bfCaptchaEntry").on("click", function(){ myFunction(); });
 $("#add-to-list").on("submit", function (event) {
@@ -98,3 +48,4 @@ $("#add-to-list").on("submit", function (event) {
   var parkToSearch = $("#list-item").val();
   getFullName(parkToSearch);
 });
+
